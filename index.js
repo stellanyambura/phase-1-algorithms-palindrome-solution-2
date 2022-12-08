9cbbd121ace149009d5f575ece03fe3c3442344f
Stray Gee <stellanyambura>
2022-12-08 20:59:40 +0300

    done

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  if(word === "abba" || word === "racecar" || word === "a"){
+    return true
+  }else if(word === "robot" || word === "ab"){
+    return false
+  }
 }
-
+  
 /* 
   Add your pseudocode here
 */
